da4bd23b36fcbff5380a72a12110fc7d49b12e46
sauravbilung <saurav.bilung@gmail.com>
2020-08-23 20:56:36 +0530

    bug fixes in writing to a file package

--- src/main/java/com/Storm/WritingToAFile/FilterFieldsToFileBolt.java
+++ src/main/java/com/Storm/WritingToAFile/FilterFieldsToFileBolt.java
@@ -14,34 +14,41 @@ import org.apache.storm.tuple.Values;
 public class FilterFieldsToFileBolt extends BaseBasicBolt {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private PrintWriter writer;
-	
-	public void Prepare(@SuppressWarnings("rawtypes") Map stormConf,TopologyContext context) {
-		
-		String fileName="output"+"-"+context.getThisTaskId()+"-"+context.getThisComponentId()+".txt";
-		
+
+	@Override
+	public void prepare(@SuppressWarnings("rawtypes") Map stormConf, TopologyContext context) {
+
+		String fileName = "output" + "-" + context.getThisTaskId() + "-" + context.getThisComponentId() + ".txt";
+
 		try {
-		
-			this.writer=new PrintWriter(stormConf.get("dirToWrite").toString()+fileName,"UTF-8");
-			
-		}catch (Exception e) {}
+
+			this.writer = new PrintWriter(stormConf.get("dirToWrite").toString() + fileName, "UTF-8");
+
+		} catch (Exception e) {
+		}
 	}
 
 	@Override
 	public void execute(Tuple input, BasicOutputCollector collector) {
 
-		String firstName=input.getStringByField("first_name");
-		String lastName=input.getString(2);
-		
-		writer.println(firstName+","+lastName);
-		collector.emit(new Values(firstName,lastName));
+		String firstName = input.getStringByField("first_name");
+		String lastName = input.getString(2);
+
+		writer.println(firstName + "," + lastName);
+		collector.emit(new Values(firstName, lastName));
 	}
 
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		
-		declarer.declare(new Fields("first_name","last_name"));
+
+		declarer.declare(new Fields("first_name", "last_name"));
+	}
+
+	@Override
+	public void cleanup() {
+		writer.close();
 	}
 
 }
